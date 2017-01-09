59ee3514297c0119d41217857c517dc4d54a1c89
DESKTOP-UCU2AKG\ASD <qzy09101018@sina.com>
2017-01-09 18:51:38 +0800

    Mod_

--- src/views/index/views/about.js
+++ src/views/index/views/about.js
@@ -17,6 +17,8 @@ class About extends React.Component {
 			{name: 2, type: 'type_3'},
 			{name: 3, type: 'type_4'}
 		];
+		this.layout = { x: 4, y: 0, w: 1, h: 2 };
+		this.saveGrid = this.saveGrid.bind(this);
 	}
 	shouldComponentUpdate() {
         return false; //告诉react这个component我们打算自己瞎搞，叫它别碰
@@ -25,12 +27,17 @@ class About extends React.Component {
 	}
 	componentDidMount() {
     }
+	saveGrid(){
+		var x = this.layout.x + 1;
+		this.layout = { x: x, y: 0, w: 1, h: 2 };
+		console.log(this.layout)
+	}
 	render(){
 		// var layout = [
 		// 	{i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
 		// 	{i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
 		// 	{i: 'c', x: 4, y: 0, w: 1, h: 2}
-		// ];	
+		// ];
 					// <ResponsiveReactGridLayout className="layout" layouts={layouts}
 					// breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
 					// cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}>
@@ -40,17 +47,26 @@ class About extends React.Component {
 					// 	<div key="4" data-grid={{w: 2, h: 3, x: 6, y: 0}}><span className="text">4</span></div>
 					// 	<div key="5" data-grid={{w: 2, h: 3, x: 8, y: 0}}><span className="text">5</span></div>
 					// </ResponsiveReactGridLayout>
-	var layouts = {};
+		var layouts = {};
 		return(
 			<section className="about-wrap">
 				<div className="template-area">
 					<Aside list={ this.list }/>
 					<ReactGridLayout className="layout" cols={12} colWidth={30} rowHeight={30} width={1200}>
-						<div key="a" data-grid={{ isResizable: true, x: 0, y: 0, w: 1, h: 2/*, static: true*/}}>a</div>
-						<div key="b" data-grid={{ isResizable: true, x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b</div>
-						<div key="c" data-grid={{ isResizable: true, x: 4, y: 0, w: 1, h: 2}}>c</div>
-						<div key="d" data-grid={{ isResizable: true, x: 4, y: 0, w: 1, h: 2}}>d</div>
+						<div key="a" data-grid={{ x: 0, y: 0, w: 1, h: 2/*, static: true*/}}>a
+							<div className="item"></div>
+						</div>
+						<div key="b" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>b
+							<div className="item"></div>
+						</div>
+						<div key="c" data-grid={{ x: 4, y: 0, w: 1, h: 2}}>c
+							<div className="item"></div>
+						</div>
+						<div key="d" ref="d" data-grid={this.layout}>d
+							<div className="item"></div>
+						</div>
 					</ReactGridLayout>
+					<button className="saveGrid" onClick={this.saveGrid}>保存选项</button>
 				</div>
 			</section >
 		)
